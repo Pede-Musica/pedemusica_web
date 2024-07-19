a77924d8d1ebabf78f27b896bb985374b6da0153
felipe.sasaki <felipe.sasaki@alaresinternet.com.br>
2024-07-19 16:00:49 -0300

    Angular Docker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false
 };
 
-export const frontUrl: string = 'http://82.112.245.110:4200';
-export const endpoint: string = 'http://82.112.245.110:3000';
+export const frontUrl: string = 'https://www.coac.cooperflow.com.br';
+export const endpoint: string = 'https://www.api-coac.cooperflow.com.br';
