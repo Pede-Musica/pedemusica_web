f3af7ab99f6f0714b982c30505eb1ce531982b97
Felipe Sasaki <felipe.sasaki_iebt@123milhas.com.br>
2025-08-03 12:20:47 -0300

    Correção de endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false
 };
 
-export const frontUrl: string = 'https://www.coac.cooperflow.com.br';
-export const endpoint: string = 'https://api-coac.cooperflow.com.br';
+export const frontUrl: string = 'https://www.app.pedemusica.com.br';
+export const endpoint: string = 'https://api.pedemusica.com.br';
